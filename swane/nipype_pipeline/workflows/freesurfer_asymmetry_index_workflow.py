947f0c956d58d3fa3eddfe726f6fc7e5a673c71d
mauriliogenovese <mauriliogenovese@gmail.com>
2023-11-27 19:38:04 +0100

    surface based asymmetry index test

--- swane/nipype_pipeline/workflows/freesurfer_asymmetry_index_workflow.py
+++ swane/nipype_pipeline/workflows/freesurfer_asymmetry_index_workflow.py
@@ -6,14 +6,13 @@ from nipype import Node, Merge
 from swane.nipype_pipeline.engine.CustomWorkflow import CustomWorkflow
 from swane.nipype_pipeline.nodes.TTest import TTest
 from swane.nipype_pipeline.nodes.SumMultiVols import SumMultiVols
-from swane.nipype_pipeline.nodes.Zscore import Zscore
+from swane.nipype_pipeline.nodes.utils import getn
 
 from nipype.interfaces.utility import IdentityInterface
 
-
 lh_labels = [
-    17,      # Left-Hippocampus                        220 216 20  0
-    18,      # Left-Amygdala                           103 255 255 0
+    17,      # Left-Hippocampus                    220 216 20  0
+    18,      # Left-Amygdala                       103 255 255 0
     1000,    # ctx-lh-unknown                      25  5   25  0
     1001,    # ctx-lh-bankssts                     25  100 40  0
     1002,    # ctx-lh-caudalanteriorcingulate      125 100 160 0
@@ -53,44 +52,44 @@ lh_labels = [
 ]
 
 labels_rh = [
-    53,     # Right - Hippocampus                       220 216 20  0
-    54,     # Right - Amygdala                          103 255 255 0
+    53,     # Right - Hippocampus                 220 216 20  0
+    54,     # Right - Amygdala                    103 255 255 0
     2000,   # ctx-rh-unknown                      25  5   25  0
-    2001,   #    ctx-rh-bankssts                     25  100 40  0
-    2002,   #    ctx-rh-caudalanteriorcingulate      125 100 160 0
-    2003,   #    ctx-rh-caudalmiddlefrontal          100 25  0   0
-    2004,   #    ctx-rh-corpuscallosum               120 70  50  0
-    2005,   #    ctx-rh-cuneus                       220 20  100 0
-    2006,   #    ctx-rh-entorhinal                   220 20  10  0
-    2007,   #   ctx-rh-fusiform                     180 220 140 0
-    2008,   #    ctx-rh-inferiorparietal             220 60  220 0
-    2009,   #    ctx-rh-inferiortemporal             180 40  120 0
-    2010,   #    ctx-rh-isthmuscingulate             140 20  140 0
-    2011,   #    ctx-rh-lateraloccipital             20  30  140 0
-    2012,   #    ctx-rh-lateralorbitofrontal         35  75  50  0
-    2013,   #    ctx-rh-lingual                      225 140 140 0
-    2014,   #    ctx-rh-medialorbitofrontal          200 35  75  0
-    2015,   #    ctx-rh-middletemporal               160 100 50  0
-    2016,   #   ctx-rh-parahippocampal              20  220 60  0
-    2017,   #    ctx-rh-paracentral                  60  220 60  0
-    2018,   #    ctx-rh-parsopercularis              220 180 140 0
-    2019,   #    ctx-rh-parsorbitalis                20  100 50  0
-    2020,   #    ctx-rh-parstriangularis             220 60  20  0
-    2021,   #    ctx-rh-pericalcarine                120 100 60  0
-    2022,   #    ctx-rh-postcentral                  220 20  20  0
-    2023,   #    ctx-rh-posteriorcingulate           220 180 220 0
-    2024,   #    ctx-rh-precentral                   60  20  220 0
-    2025,   #    ctx-rh-precuneus                    160 140 180 0
-    2026,   #    ctx-rh-rostralanteriorcingulate     80  20  140 0
-    2027,   #    ctx-rh-rostralmiddlefrontal         75  50  125 0
-    2028,   #    ctx-rh-superiorfrontal              20  220 160 0
-    2029,   #    ctx-rh-superiorparietal             20  180 140 0
-    2030,   #    ctx-rh-superiortemporal             140 220 220 0
-    2031,   #    ctx-rh-supramarginal                80  160 20  0
-    2032,   #    ctx-rh-frontalpole                  100 0   100 0
-    2033,   #    ctx-rh-temporalpole                 70  70  70  0
-    2034,   #    ctx-rh-transversetemporal           150 150 200 0
-    2035,   #    ctx-rh-insula                       255 192 32  0
+    2001,   # ctx-rh-bankssts                     25  100 40  0
+    2002,   # ctx-rh-caudalanteriorcingulate      125 100 160 0
+    2003,   # ctx-rh-caudalmiddlefrontal          100 25  0   0
+    2004,   # ctx-rh-corpuscallosum               120 70  50  0
+    2005,   # ctx-rh-cuneus                       220 20  100 0
+    2006,   # ctx-rh-entorhinal                   220 20  10  0
+    2007,   # ctx-rh-fusiform                     180 220 140 0
+    2008,   # ctx-rh-inferiorparietal             220 60  220 0
+    2009,   # ctx-rh-inferiortemporal             180 40  120 0
+    2010,   # ctx-rh-isthmuscingulate             140 20  140 0
+    2011,   # ctx-rh-lateraloccipital             20  30  140 0
+    2012,   # ctx-rh-lateralorbitofrontal         35  75  50  0
+    2013,   # ctx-rh-lingual                      225 140 140 0
+    2014,   # ctx-rh-medialorbitofrontal          200 35  75  0
+    2015,   # ctx-rh-middletemporal               160 100 50  0
+    2016,   # ctx-rh-parahippocampal              20  220 60  0
+    2017,   # ctx-rh-paracentral                  60  220 60  0
+    2018,   # ctx-rh-parsopercularis              220 180 140 0
+    2019,   # ctx-rh-parsorbitalis                20  100 50  0
+    2020,   # ctx-rh-parstriangularis             220 60  20  0
+    2021,   # ctx-rh-pericalcarine                120 100 60  0
+    2022,   # ctx-rh-postcentral                  220 20  20  0
+    2023,   # ctx-rh-posteriorcingulate           220 180 220 0
+    2024,   # ctx-rh-precentral                   60  20  220 0
+    2025,   # ctx-rh-precuneus                    160 140 180 0
+    2026,   # ctx-rh-rostralanteriorcingulate     80  20  140 0
+    2027,   # ctx-rh-rostralmiddlefrontal         75  50  125 0
+    2028,   # ctx-rh-superiorfrontal              20  220 160 0
+    2029,   # ctx-rh-superiorparietal             20  180 140 0
+    2030,   # ctx-rh-superiortemporal             140 220 220 0
+    2031,   # ctx-rh-supramarginal                80  160 20  0
+    2032,   # ctx-rh-frontalpole                  100 0   100 0
+    2033,   # ctx-rh-temporalpole                 70  70  70  0
+    2034,   # ctx-rh-transversetemporal           150 150 200 0
+    2035,   # ctx-rh-insula                       255 192 32  0
 ]
 
 
@@ -144,12 +143,13 @@ def freesurfer_asymmetry_index_workflow(name: str, base_dir: str = "/") -> Custo
 
     # Output Node
     outputnode = Node(
-        IdentityInterface(fields=['asymmetry_t', 'asymmetry_p', 'asymmetry_z']),
+        IdentityInterface(fields=['asymmetry_t', 'asymmetry_p', 'asymmetry_z', 'asymmetry_ai']),
         name='outputnode')
 
-    merge_node_t = Node(Merge(len(lh_labels)*2), name="merge_node_t")
+    merge_node_t = Node(Merge(len(lh_labels) * 2), name="merge_node_t")
     merge_node_p = Node(Merge(len(lh_labels) * 2), name="merge_node_p")
     merge_node_z = Node(Merge(len(lh_labels) * 2), name="merge_node_z")
+    merge_node_ai = Node(Merge(len(lh_labels) * 2), name="merge_node_ai")
     index = 1
 
     for lh_label in lh_labels:
@@ -192,6 +192,44 @@ def freesurfer_asymmetry_index_workflow(name: str, base_dir: str = "/") -> Custo
         workflow.connect(lh_stats, "out_stat", t_test, "stats_lh")
         workflow.connect(rh_stats, "out_stat", t_test, "stats_rh")
 
+        lh_ai_sum = Node(BinaryMaths(), name="lh_ai_sum_%d" % lh_label)
+        lh_ai_sum.inputs.operation = "add"
+        workflow.connect(inputnode, "in_file", lh_ai_sum, "in_file")
+        workflow.connect(rh_stats, ('out_stat', getn, 0), lh_ai_sum, "operand_value")
+
+        lh_ai_sub = Node(BinaryMaths(), name="lh_ai_sub_%d" % lh_label)
+        lh_ai_sub.inputs.operation = "sub"
+        workflow.connect(inputnode, "in_file", lh_ai_sub, "in_file")
+        workflow.connect(rh_stats, ('out_stat', getn, 0), lh_ai_sub, "operand_value")
+
+        lh_ai = Node(BinaryMaths(), name="lh_ai_%d" % lh_label)
+        lh_ai.inputs.operation = "div"
+        workflow.connect(lh_ai_sub, "out_file", lh_ai, "in_file")
+        workflow.connect(lh_ai_sum, "out_file", lh_ai, "operand_file")
+
+        lh_ai_mask = Node(ApplyMask(), name="lh_ai_applymask_%d" % lh_label)
+        workflow.connect(lh_ai, "out_file", lh_ai_mask, "in_file")
+        workflow.connect(lh_mask, "out_file", lh_ai_mask, "mask_file")
+
+        rh_ai_sum = Node(BinaryMaths(), name="rh_ai_sum_%d" % rh_label)
+        rh_ai_sum.inputs.operation = "add"
+        workflow.connect(inputnode, "in_file", rh_ai_sum, "in_file")
+        workflow.connect(rh_stats, ('out_stat', getn, 0), rh_ai_sum, "operand_value")
+
+        rh_ai_sub = Node(BinaryMaths(), name="rh_ai_sub_%d" % rh_label)
+        rh_ai_sub.inputs.operation = "sub"
+        workflow.connect(inputnode, "in_file", rh_ai_sub, "in_file")
+        workflow.connect(rh_stats, ('out_stat', getn, 0), rh_ai_sub, "operand_value")
+
+        rh_ai = Node(BinaryMaths(), name="rh_ai_%d" % rh_label)
+        rh_ai.inputs.operation = "div"
+        workflow.connect(rh_ai_sub, "out_file", rh_ai, "in_file")
+        workflow.connect(rh_ai_sum, "out_file", rh_ai, "operand_file")
+
+        rh_ai_mask = Node(ApplyMask(), name="rh_ai_applymask_%d" % rh_label)
+        workflow.connect(rh_ai, "out_file", rh_ai_mask, "in_file")
+        workflow.connect(rh_mask, "out_file", rh_ai_mask, "mask_file")
+
         lh_value_t = Node(BinaryMaths(), name="lh_value_t_%d" % lh_label)
         lh_value_t.inputs.operation = "mul"
         workflow.connect(lh_mask, "out_file", lh_value_t, "in_file")
@@ -228,10 +266,12 @@ def freesurfer_asymmetry_index_workflow(name: str, base_dir: str = "/") -> Custo
         workflow.connect(lh_value_t, "out_file", merge_node_t, 'in%d' % index)
         workflow.connect(lh_value_p, "out_file", merge_node_p, 'in%d' % index)
         workflow.connect(lh_z, "out_file", merge_node_z, 'in%d' % index)
+        workflow.connect(lh_ai_mask, "out_file", merge_node_ai, 'in%d' % index)
         index += 1
         workflow.connect(rh_value_t, "out_file", merge_node_t, 'in%d' % index)
         workflow.connect(lh_value_p, "out_file", merge_node_p, 'in%d' % index)
         workflow.connect(rh_z, "out_file", merge_node_z, 'in%d' % index)
+        workflow.connect(rh_ai_mask, "out_file", merge_node_ai, 'in%d' % index)
         index += 1
 
     sum_masks_t = Node(SumMultiVols(), name="sum_masks_t")
@@ -243,8 +283,12 @@ def freesurfer_asymmetry_index_workflow(name: str, base_dir: str = "/") -> Custo
     sum_masks_z = Node(SumMultiVols(), name="sum_masks_z")
     workflow.connect(merge_node_z, "out", sum_masks_z, "vol_files")
 
+    sum_masks_ai = Node(SumMultiVols(), name="sum_masks_ai")
+    workflow.connect(merge_node_ai, "out", sum_masks_ai, "vol_files")
+
     workflow.connect(sum_masks_t, "out_file", outputnode, "asymmetry_t")
     workflow.connect(sum_masks_p, "out_file", outputnode, "asymmetry_p")
     workflow.connect(sum_masks_z, "out_file", outputnode, "asymmetry_z")
+    workflow.connect(sum_masks_ai, "out_file", outputnode, "asymmetry_ai")
 
     return workflow
