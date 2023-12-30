13adcf1bc7d70f448aee9e52dedfe2cc41607d74
mauriliogenovese <mauriliogenovese@gmail.com>
2023-12-30 07:35:47 +0100

    typo

--- swane/strings.py
+++ swane/strings.py
@@ -159,7 +159,7 @@ check_dep_slicer_error1 = "Slicer not detected (<a href='https://slicer.readthed
                           "/getting_started.html#installing-3d-slicer/'>Installation info</a>)"
 check_dep_slicer_error2 = "Slicer detected but without SlicerFreeSurfer extension (<a " \
                           "href='https://slicer.readthedocs.io/en/latest/user_guide/extensions_manager.html?highlight" \
-                          "=extension%20manager'>Exstensions Manager info</a>)"
+                          "=extension%20manager'>Extensions Manager info</a>)"
 check_dep_slicer_wrong_version = "Slicer version outdated (found %s, required %s). Please " \
                               "<a href='https://slicer.readthedocs.io/en/latest/user_guide'>update</a>"
 check_dep_slicer_found = "Slicer detected (%s)"
