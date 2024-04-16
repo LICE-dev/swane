6be4a985f02d7ed3ed3b4c5ef4a8c6ee2523a211
Agostino_Arcasensa <powerictsoft@gmail.com>
2024-04-16 17:01:22 +0200

    Update swane_venv_creator.sh

--- swane_venv_creator.sh
+++ swane_venv_creator.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
+# Deactivating running virtual env, if any
+deactivate
+
+cd $HOME
+venv_name="swane_venv"
+python_version="python3"
+
 # Checking the FSL/FREESURFER user profile configuration
-python_check=$(which python3)
-if [[ python_check = *^fsl^* ]]; then
+echo "Checking User Profile configuration..."
+python_path=$(which $python_version)
+if [[ $python_path = *^fsl^* ]]; then
     echo "Wrong FSL and FREESURFER configuration in your profile"
     echo "Please fix it before installing SWANe"
     echo "More info on https://github.com/LICE-dev/swane/wiki/04-Dependencies-Guides#freesurferfsl-conflict-with-python"
     exit 1
 fi
 
-# Create a swane_env virtual environment dedicated to SWANe using the python3 interpreter
-cd $HOME
-venv_name="swane_venv"
-python_version="python3"
-
+# Creating a swane_env virtual environment dedicated to SWANe using the python3 interpreter
 "${python_version}" -m venv "$venv_name"
 
-# Activate the swane_venv
+# Activating the swane_venv
 source "$HOME/$venv_name/bin/activate"
 
-# Installing the SWANe package using pip
+# Installing SWANe package using pip
 "${python_version}" -m pip install swane
 
-# Deactivate the swane_venv
+# Deactivating swane_venv
 deactivate
 
-# Create a script to activate the virtual env and execute the command python -m swane
+# Creating a script to activate the virtual env and execute the command python -m swane
 echo "#!/bin/bash" > run_swane.sh
 echo "source $HOME/$venv_name/bin/activate" >> run_swane.sh
 echo "${python_version} -m swane" >> run_swane.sh
 chmod +x run_swane.sh
 
 echo "SWANe Virtual Environment created and configurated"
-echo "Execute './run_swane.sh' to activate the swane_venv and start SWANe"
-echo "Checking User Profile configuration..."
-
+echo "To activate swane_venv and start SWANe please run the following command: $HOME/run_swane.sh"
