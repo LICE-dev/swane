f2ef4c0d9d5edd9a8759e4463503c935a39f5e74
Agostino_Arcasensa <powerictsoft@gmail.com>
2024-11-11 08:01:41 +0100

    Update DicomSearchWorker.py

--- swane/workers/DicomSearchWorker.py
+++ swane/workers/DicomSearchWorker.py
@@ -14,6 +14,7 @@ class DicomSearchWorker(QRunnable):
     def __init__(self, dicom_dir: str):
         """
         Thread class to scan a dicom folder and return dicom files ordered in subjects, exams and series
+
         Parameters
         ----------
         dicom_dir: str
@@ -31,6 +32,7 @@ class DicomSearchWorker(QRunnable):
     def clean_text(string: str) -> str:
         """
         Remove forbidden characters from a string
+
         Parameters
         ----------
         string: str
@@ -42,7 +44,20 @@ class DicomSearchWorker(QRunnable):
 
         """
         # clean and standardize text descriptions, which makes searching files easier
-        forbidden_symbols = ["*", ".", ",", "\"", "\\", "/", "|", "[", "]", ":", ";", " "]
+        forbidden_symbols = [
+            "*",
+            ".",
+            ",",
+            '"',
+            "\\",
+            "/",
+            "|",
+            "[",
+            "]",
+            ":",
+            ";",
+            " ",
+        ]
         for symbol in forbidden_symbols:
             # replace everything with an underscore
             string = string.replace(symbol, "_")
@@ -52,7 +67,11 @@ class DicomSearchWorker(QRunnable):
         """
         Generates the list of file to be scanned.
         """
-        if self.dicom_dir is None or self.dicom_dir == "" or not os.path.exists(self.dicom_dir):
+        if (
+            self.dicom_dir is None
+            or self.dicom_dir == ""
+            or not os.path.exists(self.dicom_dir)
+        ):
             return
         self.unsorted_list = []
         for root, dirs, files in os.walk(self.dicom_dir):
@@ -94,13 +113,23 @@ class DicomSearchWorker(QRunnable):
                 study_instance_uid = ds.get("StudyInstanceUID", "NA")
 
                 # in GE la maggior parte delle ricostruzioni sono DERIVED\SECONDARY
-                if hasattr(ds, 'ImageType') and "DERIVED" in ds.ImageType and "SECONDARY" in ds.ImageType and "ASL" not in ds.ImageType:
+                if (
+                    hasattr(ds, "ImageType")
+                    and "DERIVED" in ds.ImageType
+                    and "SECONDARY" in ds.ImageType
+                    and "ASL" not in ds.ImageType
+                ):
                     continue
                 # in GE e SIEMENS l'immagine anatomica di ASL è ORIGINAL\PRIMARY\ASL
-                if hasattr(ds, 'ImageType') and "ORIGINAL" in ds.ImageType and "PRIMARY" in ds.ImageType and "ASL" in ds.ImageType:
+                if (
+                    hasattr(ds, "ImageType")
+                    and "ORIGINAL" in ds.ImageType
+                    and "PRIMARY" in ds.ImageType
+                    and "ASL" in ds.ImageType
+                ):
                     continue
                 # in Philips e Siemens le ricostruzioni sono PROJECTION IMAGE
-                if hasattr(ds, 'ImageType') and "PROJECTION IMAGE" in ds.ImageType:
+                if hasattr(ds, "ImageType") and "PROJECTION IMAGE" in ds.ImageType:
                     continue
 
                 if subject_id not in self.dicom_tree:
@@ -117,14 +146,27 @@ class DicomSearchWorker(QRunnable):
 
                 if series_number not in self.dicom_tree[subject_id][study_instance_uid]:
                     self.dicom_tree[subject_id][study_instance_uid][series_number] = []
-                    self.series_positions[subject_id][study_instance_uid][series_number] = [ds.get("SliceLocation"), 0]
+                    self.series_positions[subject_id][study_instance_uid][
+                        series_number
+                    ] = [ds.get("SliceLocation"), 0]
                     if DEBUG:
-                        print("New series: " + str(series_number) + " " + ds.SeriesDescription)
+                        print(
+                            "New series: "
+                            + str(series_number)
+                            + " "
+                            + ds.SeriesDescription
+                        )
 
-                self.dicom_tree[subject_id][study_instance_uid][series_number].append(dicom_loc)
+                self.dicom_tree[subject_id][study_instance_uid][series_number].append(
+                    dicom_loc
+                )
 
-                if self.series_positions[subject_id][study_instance_uid][series_number][0] == ds.get("SliceLocation"):
-                    self.series_positions[subject_id][study_instance_uid][series_number][1] += 1
+                if self.series_positions[subject_id][study_instance_uid][series_number][
+                    0
+                ] == ds.get("SliceLocation"):
+                    self.series_positions[subject_id][study_instance_uid][
+                        series_number
+                    ][1] += 1
                     if DEBUG:
                         print("New volume for series: " + str(series_number))
 
@@ -141,10 +183,12 @@ class DicomSearchWorker(QRunnable):
     def get_exam_list(self, subject: str) -> list[pydicom.uid.UID]:
         """
         Extract from dicom search the exams of specified subject and return their study_id
+
         Parameters
         ----------
         subject: str
             The subject id
+
         Returns
         -------
             A list of study_id
@@ -153,15 +197,19 @@ class DicomSearchWorker(QRunnable):
             return []
         return list(self.dicom_tree[subject].keys())
 
-    def get_series_list(self, subject: str, exam: pydicom.uid.UID) -> list[pydicom.valuerep.IS]:
+    def get_series_list(
+        self, subject: str, exam: pydicom.uid.UID
+    ) -> list[pydicom.valuerep.IS]:
         """
         Extract from dicom search the series of a specified exam of specified subject and return their series_id
+
         Parameters
         ----------
         subject: str
             The subject id
         exam: pydicom.uid.UID
             The exam id
+
         Returns
         -------
             A list of series_id
@@ -172,9 +220,12 @@ class DicomSearchWorker(QRunnable):
             return []
         return list(self.dicom_tree[subject][exam].keys())
 
-    def get_series_nvol(self, subject: str, exam: pydicom.uid.UID, series: pydicom.valuerep.IS) -> int:
+    def get_series_nvol(
+        self, subject: str, exam: pydicom.uid.UID, series: pydicom.valuerep.IS
+    ) -> int:
         """
         Extract from dicom search the number of volumes of a specified series of a specified exam of specified subject and return their series_id
+
         Parameters
         ----------
         subject: str
@@ -183,15 +234,19 @@ class DicomSearchWorker(QRunnable):
             The exam id
         series: pydicom.valuerep.IS
             The series id
+
         Returns
         -------
             An integer corresponding to the number of volumes of wanted series
         """
         return self.series_positions[subject][exam][series][1]
 
-    def get_series_files(self, subject: str, exam: pydicom.uid.UID, series: pydicom.valuerep.IS) -> list[str]:
+    def get_series_files(
+        self, subject: str, exam: pydicom.uid.UID, series: pydicom.valuerep.IS
+    ) -> list[str]:
         """
         Extract from dicom search the dicom file path of a specified series of a specified exam of specified subject and return their series_id
+
         Parameters
         ----------
         subject: str
@@ -200,6 +255,7 @@ class DicomSearchWorker(QRunnable):
             The exam id
         series: pydicom.valuerep.IS
             The series id
+
         Returns
         -------
             A list of series_id
@@ -212,9 +268,12 @@ class DicomSearchWorker(QRunnable):
             return []
         return list(self.dicom_tree[subject][exam][series])
 
-    def get_series_info(self, subject: str, exam: pydicom.uid.UID, series: pydicom.valuerep.IS) -> tuple[list[str], str, str, str, int]:
+    def get_series_info(
+        self, subject: str, exam: pydicom.uid.UID, series: pydicom.valuerep.IS
+    ) -> tuple[list[str], str, str, str, int]:
         """
         Extract information from dicom search the dicom file path of a specified series of a specified exam of specified subject
+
         Parameters
         ----------
         subject: str
@@ -241,13 +300,43 @@ class DicomSearchWorker(QRunnable):
         ds = pydicom.dcmread(image_list[0], force=True)
 
         # Excludes series with less than 10 images unless they are siemens mosaics series
-        if len(image_list) < 10 and hasattr(ds, 'ImageType') and "MOSAIC" not in ds.ImageType:
+        if (
+            len(image_list) < 10
+            and hasattr(ds, "ImageType")
+            and "MOSAIC" not in ds.ImageType
+        ):
             return None, None, None, None, None
 
         mod = ds.Modality
         vols = self.get_series_nvol(subject, exam, series)
         subject_name = str(ds.PatientName)
-        series_description = ds.SeriesDescription if hasattr(ds, "SeriesDescription") else "unnamed_series"
+        series_description = self.get_series_description(ds)
 
         return image_list, subject_name, mod, series_description, vols
-    
+
+    def get_series_description(self, ds: pydicom.FileDataset) -> str:
+        """
+        Extract the description of a dicom file.
+        The description is equal to:
+        - the SeriesDescription tag, if any
+        - otherwise, the file_name tag, if any
+        - otherwise, None (unnamed_series)
+
+        Parameters
+        ----------
+        ds: pydicom.FileDataset
+            The dicom file to check
+
+        Returns
+        -------
+        str
+            The dicom file description
+
+        """
+        
+        if hasattr(ds, "SeriesDescription"):
+            return ds.SeriesDescription
+        elif hasattr(ds, "filename"):
+            return os.path.basename(ds.filename)
+        else:
+            return "unnamed_series"
