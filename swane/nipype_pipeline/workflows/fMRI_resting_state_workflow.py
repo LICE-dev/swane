130a0cdf423c691c4d46ecc39a39283c72a2ef41
mauriliogenovese <125388969+mauriliogenovese@users.noreply.github.com>
2026-01-04 21:01:23 +0100

    test registration

--- swane/nipype_pipeline/workflows/fMRI_resting_state_workflow.py
+++ swane/nipype_pipeline/workflows/fMRI_resting_state_workflow.py
@@ -1,13 +1,23 @@
 from nipype import Node, IdentityInterface, SelectFiles, Merge
 from nipype.interfaces.fsl import (
     MELODIC,
-    ApplyXFM
+    ApplyXFM,
+    ImageMaths,
+    FLIRT,
+    FNIRT,
+    ApplyWarp,
+    InvWarp,
+    ConvertXFM
+
 )
 from configparser import SectionProxy
 from swane.nipype_pipeline.engine.CustomWorkflow import CustomWorkflow
 from swane.nipype_pipeline.workflows.fMRI_preproc_workflow import fMRI_preproc_workflow
 from swane.config.config_enums import SLICE_TIMING
-
+from ica_aroma_py.services.ICA_AROMA_nodes import (FeatureTimeSeries, FeatureFrequency,
+                              AromaClassification, FeatureSpatial, FeatureSpatialPrep, IsoResample)
+from ica_aroma_py import aroma_mask_out, aroma_mask_edge, aroma_mask_csf
+import os
 
 def fMRI_resting_state_workflow(
     name: str, dicom_dir: str, config: SectionProxy, base_dir: str = "/"
@@ -95,14 +105,111 @@ def fMRI_resting_state_workflow(
     workflow.connect(melodic, "out_dir", melodic_output, "melodic_dir")
     workflow.connect(melodic, "out_dir", melodic_output, "base_directory")
 
-    IC_2_ref = Node(ApplyXFM(), name="IC_2_ref")
-    IC_2_ref.long_name = "IC to reference space"
-    IC_2_ref.inputs.out_file = "r-melodic_IC.nii.gz"
-    IC_2_ref.inputs.interp = "trilinear"
-    workflow.connect(melodic_output, "IC", IC_2_ref, "in_file")
-    workflow.connect(flirt_2_ref, "out_matrix_file", IC_2_ref, "in_matrix_file")
-    workflow.connect(inputnode, "ref_brain", IC_2_ref, "reference")
+    mask_maths = Node(ImageMaths(), name="create_mask_maths")
+    mask_maths.inputs.op_string = '-Tstd -bin'
+    mask_maths.inputs.out_file = "brain_mask.nii.gz"
+    workflow.connect(highpass, "out_file", mask_maths, "in_file")
 
-    workflow.connect(IC_2_ref, "out_file", outputnode, "IC")
+    feature_spatial_prep = Node(FeatureSpatialPrep(), name="feature_spatial_prep")
+    workflow.connect(melodic_output, "thresh_zstat_files", feature_spatial_prep, "in_files")
+    workflow.connect(mask_maths, "out_file", feature_spatial_prep, "mask_file")
+
+
+    ###########################################
+
+    mni2 = os.path.join(os.environ["FSLDIR"], 'data', 'standard', 'MNI152_T1_2mm_brain.nii.gz')
+
+    flirt = Node(FLIRT(), name="ref_2_mni_flirt")
+    flirt.long_name = "%s to atlas"
+    flirt.inputs.searchr_x = [-90, 90]
+    flirt.inputs.searchr_y = [-90, 90]
+    flirt.inputs.searchr_z = [-90, 90]
+    flirt.inputs.dof = 12
+    flirt.inputs.cost = "corratio"
+    flirt.inputs.out_matrix_file = "ref_2_mni.mat"
+    flirt.inputs.reference = mni2
+    workflow.connect(inputnode, "ref_brain", flirt, "in_file")
+
+    # NODE 2: Nonlinear registration
+    fnirt = Node(FNIRT(), name="ref_2_mni_fnirt")
+    fnirt.long_name = "%s to atlas"
+    fnirt.inputs.fieldcoeff_file = True
+    fnirt.inputs.ref_file = mni2
+    workflow.connect(flirt, "out_matrix_file", fnirt, "affine_file")
+    workflow.connect(inputnode, "ref_brain", fnirt, "in_file")
+
+    # NODE 3: Inverse matrix
+    invwarp = Node(InvWarp(), name="ref_2_mni_invwarp")
+    invwarp.long_name = "%s from atlas"
+    workflow.connect(fnirt, "fieldcoeff_file", invwarp, "warp")
+    workflow.connect(inputnode, "ref_brain", invwarp, "reference")
+
+    apply_warp = Node(ApplyWarp(), name="func2mni")
+    apply_warp.inputs.ref_file = mni2
+    workflow.connect(flirt_2_ref, "out_matrix_file", apply_warp, "premat")
+    workflow.connect(feature_spatial_prep, "out_file", apply_warp, "in_file")
+    workflow.connect(fnirt, "fieldcoeff_file", apply_warp, "field_file")
+
+    ref_2_func = Node(ConvertXFM(), name="ref_2_func")
+    ref_2_func.inputs.invert_xfm = True
+    workflow.connect(flirt_2_ref, "out_matrix_file", ref_2_func, "in_file")
+
+    csf2func = Node(ApplyWarp(), name="csf2func")
+    csf2func.inputs.in_file = aroma_mask_csf
+    workflow.connect(feature_spatial_prep, "out_file", csf2func, "ref_file")
+    workflow.connect(ref_2_func, "out_file", csf2func, "postmat")
+    workflow.connect(invwarp, "inverse_warp", csf2func, "field_file")
+
+    edge2func = Node(ApplyWarp(), name="edge2func")
+    edge2func.inputs.in_file = aroma_mask_edge
+    workflow.connect(feature_spatial_prep, "out_file", edge2func, "ref_file")
+    workflow.connect(ref_2_func, "out_file", edge2func, "postmat")
+    workflow.connect(invwarp, "inverse_warp", edge2func, "field_file")
+
+    out2func = Node(ApplyWarp(), name="out2func")
+    out2func.inputs.in_file = aroma_mask_out
+    workflow.connect(feature_spatial_prep, "out_file", out2func, "ref_file")
+    workflow.connect(ref_2_func, "out_file", out2func, "postmat")
+    workflow.connect(invwarp, "inverse_warp", out2func, "field_file")
+
+    feature_spatial2 = Node(FeatureSpatial(), name="feature_spatial2")
+    workflow.connect(csf2func, "out_file", feature_spatial2, "mask_csf")
+    workflow.connect(edge2func, "out_file", feature_spatial2, "mask_edge")
+    workflow.connect(out2func, "out_file", feature_spatial2, "mask_out")
+    workflow.connect(feature_spatial_prep, "out_file", feature_spatial2, "in_file")
+
+
+
+    ##########################################################
+
+    feature_spatial = Node(FeatureSpatial(), name="feature_spatial")
+    feature_spatial.inputs.mask_csf = aroma_mask_csf
+    feature_spatial.inputs.mask_edge = aroma_mask_edge
+    feature_spatial.inputs.mask_out = aroma_mask_out
+    workflow.connect(apply_warp, "out_file", feature_spatial, "in_file")
+
+    feature_time_series = Node(FeatureTimeSeries(), name="feature_time_series")
+    workflow.connect(motion_correct, "par_file", feature_time_series, "mc")
+    workflow.connect(melodic_output, "mel_mix", feature_time_series, "mel_mix")
+
+    feature_frequency = Node(FeatureFrequency(), name="feature_frequency")
+    workflow.connect(getTR, "TR", feature_frequency, "TR")
+    workflow.connect(melodic_output, "mel_ft_mix", feature_frequency, "mel_ft_mix")
+
+    aroma_classification = Node(AromaClassification(), name="aroma_classification")
+    workflow.connect(feature_frequency, "HFC", aroma_classification, "HFC")
+    workflow.connect(feature_time_series, "max_rp_corr", aroma_classification, "max_rp_corr")
+    workflow.connect(feature_spatial, "csf_fract", aroma_classification, "csf_fract")
+    workflow.connect(feature_spatial, "edge_fract", aroma_classification, "edge_fract")
+
+    aroma_classification2 = Node(AromaClassification(), name="aroma_classification2")
+    workflow.connect(feature_frequency, "HFC", aroma_classification2, "HFC")
+    workflow.connect(feature_time_series, "max_rp_corr", aroma_classification2, "max_rp_corr")
+    workflow.connect(feature_spatial2, "csf_fract", aroma_classification2, "csf_fract")
+    workflow.connect(feature_spatial2, "edge_fract", aroma_classification2, "edge_fract")
+
+    #workflow.connect(aroma_classification, "feature_scores", outputnode, "ica_aroma_results.@feature_scores")
+    #workflow.connect(aroma_classification, "classified_motion_ics", aroma_datasink, "ica_aroma_results.@classified_motion_ics")
+    workflow.connect(aroma_classification2, "classification_overview", outputnode, "IC")
 
     return workflow
